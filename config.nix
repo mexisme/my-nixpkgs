9a42d6bc54fe605b57816dbdacf2f292607a1c49
mexisme <wildjim+dev@kiwinet.org>
2016-10-29 18:41:57 +1300

    `cargo` doesn't yet compile on OSX

--- config.nix
+++ config.nix
@@ -120,7 +120,7 @@
     my-rustlang-tools = with pkgs; buildEnv {
       name = "my-rustlang-tools";
       paths = [
-        cargo
+        #cargo
         rustc
         #rustfmt   ## Can't compile rustc!
       ];
